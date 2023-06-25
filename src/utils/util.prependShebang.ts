d94bcdbf497300839a0cee79b5c260f96dbba414
bigyanse <contact.bigyanse@gmail.com>
2023-06-25 20:41:00 +0545

    `add`: ➕ added package

---
+++ src/utils/util.prependShebang.ts
@@ -0,0 +1,13 @@
+import fs from "fs";
+import path from "path";
+
+const fileName = path.resolve(__dirname, "../cli.js");
+const data = fs.readFileSync(fileName).toString().split("\n");
+data.splice(0, 0, "#!/usr/bin/env node");
+const text = data.join("\n");
+
+try {
+  fs.writeFileSync(fileName, text);
+} catch(err) {
+  console.log("Something went wrong, please reinstall! If error exists contact developer!");
+}
